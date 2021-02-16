c8fc88ed6990cfa3da5f4095609497ce66b07de3
bea <bdeotto@gmail.com>
2021-02-16 20:21:55 +0100

    Recuperación de diccionarios resultantes de inspección

---
+++ lower_case_dict.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+
+df_dict=pd.read_csv('data/D_dict.csv',index_col=0)
+df_describe=pd.read_csv('data/D_describe.csv',index_col=0)
+
+d_dict=dict()
+for variable in df_dict.columns:
+    d_dict[variable]=dict()
+    dfv=df_dict[variable].dropna() # Solo sobre la columna relevante para eliminar no informados
+    dfv=dfv.drop_duplicates()
+    for key in dfv.index:
+        value=dfv.loc[key]
+        d_dict[variable][key]=value
+        if variable=str(variable).upper():
+            try:
+                key_n=int(key)
+                d_dict[variable][key_n]=value
+            except: pass
+
+d_describe=dict()
+for key in df_describe.index:
+    d_describe[key]=df_describe['valor'].loc[key]
